f97df7a2d992681b5185d2dbafba49d8fda19146
Bilal Shaikh <your-email@gmail.com>
2026-06-10 16:38:16 +0530

    day-1

--- day-1/src/main.js
+++ day-1/src/main.js
@@ -2,11 +2,33 @@ import gsap from "gsap";
 import "./style.css"
 
 
-gsap.fromTo(".box",{
-y:500
-}, { 
-  duration: 1,
-  x: 200,
+// Animates current state → target state
+gsap.to(".box", { 
+  delay: 1.5,
+  duration: 2,
+  x: 400,
   rotation: 360,
-  ease:"power2.inOut"
+});
+
+// Animates from custom start state → end state
+gsap.fromTo(".box", {
+  y: 0,
+  x: 0
+}, { 
+  delay: 1.5,
+  duration: 3,
+  x: 200,
+  y: 500,
+  rotation: 360,
+});
+
+// Instantly sets values (no animation, delay/duration ignored)
+gsap.set(".box", { 
+  delay: 1,      // ignored
+  duration: 4,   // ignored
+  x: 300,
+  y: 300,
+  borderRadius: "50%",
+  scale: 1.5,
+  rotation: 360,
 });
